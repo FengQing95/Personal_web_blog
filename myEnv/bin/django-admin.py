10d7ccafe8e5d1b6bd1f8acb6c20b3f37b0e41a8
Feng <qingf1995@gmail.com>
2020-02-26 03:34:49 -0600

    Jobs app up and running

---
+++ myEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/feng/Documents/Study/Online_Courses/Django/portfolio-project/myEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
